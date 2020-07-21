73a75b85f0cf3315495b152e15293112349aa811
Flloki5 <rozli@op.pl>
2020-07-21 20:48:50 +0200

    add cart class methods template

---
+++ src/main/java/com/codecool/Cart.java
@@ -0,0 +1,31 @@
+package com.codecool;
+
+import java.util.Map;
+
+public class Cart {
+    //private final Map<Product, User> products;
+
+    public void add(Product product) {
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public void remove(int i){
+
+    }
+
+    public void removeAll() {
+
+    }
+
+    public void editProduct(String key, int i){
+
+    }
+
+    //public Map<String, Integer> getProductsFromCart(){
+    //   return products;
+    //}
+}
