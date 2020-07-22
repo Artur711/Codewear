b732617f53af0f0f338748dd5cb3fbaa7e57fddc
Michal Bartosik <msbartosik@gmail.com>
2020-07-22 19:18:15 +0200

    connection instaed of datasource

--- src/main/java/com/codecool/dao/PSQLCartDao.java
+++ src/main/java/com/codecool/dao/PSQLCartDao.java
@@ -1,24 +1,23 @@
 package com.codecool.dao;
 
-import com.codecool.datasource.SQLDataSource;
 import com.codecool.model.Cart;
 
-
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class PSQLCartDao implements CartDao{
 
-    SQLDataSource dataSource;
+    Connection conn;
 
-    public PSQLCartDao(SQLDataSource dataSource) {
-        this.dataSource = dataSource;
+    public PSQLCartDao(Connection conn) {
+        this.conn = conn;
     }
 
     @Override
     public int addUserCart(Cart cart) {
         String sql = "INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, ?)";
-        try(PreparedStatement st = dataSource.connect().getConnection().prepareStatement(sql)) {
+        try(PreparedStatement st = conn.prepareStatement(sql)) {
             st.setString(1, cart.getUser_id());
             st.setString(2, cart.getProduct_id());
             st.setInt(3, cart.getQuantity());
