8caf85fabacbdde263e408d8515cf7302df5bf29
Michal Bartosik <msbartosik@gmail.com>
2020-07-22 16:23:50 +0200

    refactor

--- src/main/java/com/codecool/datasource/PostgresSQLDataSource.java
+++ src/main/java/com/codecool/datasource/PostgresSQLDataSource.java
@@ -5,22 +5,16 @@ import org.postgresql.ds.PGSimpleDataSource;
 import javax.sql.DataSource;
 
 public class PostgresSQLDataSource implements SQLDataSource {
-    private final String databaseName;
-    private final String user;
-    private final String password;
-    PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+    private final PGSimpleDataSource dataSource = new PGSimpleDataSource();
 
     public PostgresSQLDataSource(String databaseName, String user, String password) {
-        this.databaseName = databaseName;
-        this.user = user;
-        this.password = password;
+        dataSource.setDatabaseName(databaseName);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
     }
 
     public DataSource connect() {
-        dataSource.setDatabaseName(this.databaseName);
-        dataSource.setUser(this.user);
-        dataSource.setPassword(this.password);
-
         return dataSource;
     }
 }
