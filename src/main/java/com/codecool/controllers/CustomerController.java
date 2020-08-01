fec0f58c919d30c067a947b39c83c78f5bcf2f1d
Artur <arturkubiak9@gmail.com>
2020-08-01 14:00:56 +0200

    Add a font in print details

--- src/main/java/com/codecool/controllers/CustomerController.java
+++ src/main/java/com/codecool/controllers/CustomerController.java
@@ -71,6 +71,7 @@ public class CustomerController {
                             //int quantity = scanner.nextInt();
                             if(cartDao.isAvailableOnStock(product.getQuantity(), quantity) && quantity > 0){
                                 cartDao.add(user.getId(), product.getId(), quantity);
+                                System.out.println(colorize("  Added to cart " + product.getQuantity(), mainView.PROMPT_FORMAT));
                             }else{
                                 System.out.println(colorize("  Quantity available in stock: " + product.getQuantity(), mainView.PROMPT_FORMAT));
                             }
